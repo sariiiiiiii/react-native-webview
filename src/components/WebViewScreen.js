d4fadcabac74338174ade71ec8988d718e8638a9
sariiiiiiii <abdf13423@naver.com>
2024-08-01 15:12:00 +0900

    modified WebViewScreen.js

--- src/components/WebViewScreen.js
+++ src/components/WebViewScreen.js
@@ -1,31 +1,43 @@
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Dimensions } from 'react-native';
+import React, { useRef, useState } from "react";
+import { StyleSheet, Dimensions, Button, View } from "react-native";
 import WebView from 'react-native-webview';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const WebViewScreen = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [showWebView, setShowWebView] = useState(true);
+  const webViewRef = useRef(null);
 
-  const onReceiveDateFromWeb = (event) => {
+  const handleMessage = (event) => {
+    console.log("Url:", event.nativeEvent.url);
+    console.log("Title:", event.nativeEvent.title);
+    console.log("Data:", event.nativeEvent.data);
+
     const data = JSON.parse(event.nativeEvent.data);
     console.log("Data:", data);
 
-    if (data["action"] === "Website2Mobile") {
-      const isOpen = data["payload"]["isOpen"];
-      setIsOpen(isOpen);
+    if (data.action === "Website2Mobile") {
+      const { isOpen } = data.payload;
+      console.log("Received data from web:", data.payload);
+      setShowWebView(isOpen);  // 웹뷰 닫기 또는 열기
     }
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <WebView
-        style={styles.webview}
-        source={{ uri: 'https://edoc.postline.co.kr/contract/01?q=0D91E01B194B2583461CE1A686AADE9C' }}
-        onMessage={onReceiveDateFromWeb}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      {showWebView && (
+        <WebView
+          ref={webViewRef}
+          style={styles.webview}
+          source={{ uri: 'https://edoc.postline.co.kr/contract/01?q=83F064F52434CF5269E28757A0458CB8' }}
+          onMessage={handleMessage}
+        />
+      )}
+      {!showWebView && (
+        <Button onPress={() => setShowWebView(true)} title="Open WebView" />
+      )}
+    </View>
   );
 };
 
